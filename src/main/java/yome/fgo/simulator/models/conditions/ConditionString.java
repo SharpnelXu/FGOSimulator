5df45e3b813979013bc65b9469b6d9bdc73f98ae
Yome <695312637@qq.com>
2022-04-23 14:41:52 -0700

    Delete unused classes

--- src/main/java/yome/fgo/simulator/models/conditions/ConditionString.java
+++
@@ -1,4 +0,0 @@
-package yome.fgo.simulator.models.conditions;
-
-public class ConditionString {
-}
--- src/main/java/yome/fgo/simulator/models/mysticcodes/MysticCodeData.java
+++
@@ -1,7 +0,0 @@
-package yome.fgo.simulator.models.mysticcodes;
-
-public class MysticCodeData {
-    public MysticCode buildMysticCode() {
-        return new MysticCode();
-    }
-}
