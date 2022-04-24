37fdba27ba76a3e37a0b835ec30b91080c03855c
Yome <695312637@qq.com>
2022-04-23 20:43:13 -0700

    NoblePhantasmType

--- src/main/java/yome/fgo/simulator/models/combatants/NoblePhantasm.java
+++ src/main/java/yome/fgo/simulator/models/combatants/NoblePhantasm.java
@@ -2,6 +2,7 @@ package yome.fgo.simulator.models.combatants;
 
 import lombok.Getter;
 import yome.fgo.data.proto.FgoStorageData.NoblePhantasmData;
+import yome.fgo.data.proto.FgoStorageData.NoblePhantasmType;
 import yome.fgo.simulator.models.Simulation;
 import yome.fgo.simulator.models.effects.Effect;
 import yome.fgo.simulator.models.effects.EffectFactory;
@@ -11,10 +12,12 @@ import java.util.List;
 @Getter
 public class NoblePhantasm extends CommandCard {
     private final List<Effect> effects;
+    private final NoblePhantasmType noblePhantasmType;
 
     public NoblePhantasm(final NoblePhantasmData noblePhantasmData, final int noblePhantasmLevel) {
         super(noblePhantasmData.getCommandCardData());
         this.effects = EffectFactory.buildEffects(noblePhantasmData.getEffectsList(), noblePhantasmLevel);
+        this.noblePhantasmType = noblePhantasmData.getNoblePhantasmType();
     }
 
     public void activate(final Simulation simulation, final int overchargeLevel) {
---
+++ src/main/java/yome/fgo/simulator/models/effects/NoblePhantasmDamage.java
@@ -0,0 +1,13 @@
+package yome.fgo.simulator.models.effects;
+
+import lombok.experimental.SuperBuilder;
+import yome.fgo.simulator.models.Simulation;
+
+@SuperBuilder
+public class NoblePhantasmDamage extends Effect {
+
+    @Override
+    public void apply(Simulation simulation, int level) {
+
+    }
+}
