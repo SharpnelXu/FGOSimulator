815b694ddf0401007ea6623d106c0293709fa46d
Yome <695312637@qq.com>
2022-04-26 10:39:23 -0700

    Remove unused import

--- src/main/java/yome/fgo/simulator/models/craftessences/CraftEssence.java
+++ src/main/java/yome/fgo/simulator/models/craftessences/CraftEssence.java
@@ -3,7 +3,6 @@ package yome.fgo.simulator.models.craftessences;
 import lombok.Getter;
 import yome.fgo.data.proto.FgoStorageData.CraftEssenceData;
 import yome.fgo.data.proto.FgoStorageData.CraftEssenceOption;
-import yome.fgo.data.proto.FgoStorageData.Status;
 import yome.fgo.simulator.models.combatants.Skill;
 
 @Getter
