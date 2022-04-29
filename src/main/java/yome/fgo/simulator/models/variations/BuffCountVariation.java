50622c9858fe448fcef40a3427b5160c0b745423
Yome <695312637@qq.com>
2022-04-29 14:54:30 -0700

    Cleanup previous variations

--- src/main/java/yome/fgo/simulator/models/variations/BuffCountVariation.java
+++ src/main/java/yome/fgo/simulator/models/variations/BuffCountVariation.java
@@ -3,9 +3,11 @@ package yome.fgo.simulator.models.variations;
 import lombok.AllArgsConstructor;
 import yome.fgo.data.proto.FgoStorageData.Target;
 import yome.fgo.simulator.models.Simulation;
+import yome.fgo.simulator.models.combatants.Combatant;
 import yome.fgo.simulator.models.conditions.Condition;
+import yome.fgo.simulator.models.effects.buffs.Buff;
 import yome.fgo.simulator.utils.RoundUtils;
-import yome.fgo.simulator.utils.SpecificDamageUtils;
+import yome.fgo.simulator.utils.TargetUtils;
 
 @AllArgsConstructor
 public class BuffCountVariation implements Variation {
@@ -15,7 +17,32 @@ public class BuffCountVariation implements Variation {
 
     @Override
     public double evaluate(final Simulation simulation, final double baseValue, final double additionValue) {
-        final int count = SpecificDamageUtils.countBuff(simulation, target, matchCondition, maxCount);
+        final int count = countBuff(simulation, target, matchCondition, maxCount);
         return  RoundUtils.roundNearest(baseValue + additionValue * count);
+    }
+
+
+    public static int countBuff(
+            final Simulation simulation,
+            final Target target,
+            final Condition matchCondition,
+            final int maxCount
+    ) {
+        int count = 0;
+        for (final Combatant combatant : TargetUtils.getTargets(simulation, target)) {
+            for (final Buff buff : combatant.getBuffs()) {
+                simulation.setCurrentBuff(buff);
+
+                if (matchCondition.evaluate(simulation)) {
+                    count++;
+                    if (maxCount == count) {
+                        return maxCount;
+                    }
+                }
+
+                simulation.unsetCurrentBuff();
+            }
+        }
+        return count;
     }
 }
--- src/main/java/yome/fgo/simulator/models/variations/TraitCountVariation.java
+++ src/main/java/yome/fgo/simulator/models/variations/TraitCountVariation.java
@@ -3,8 +3,9 @@ package yome.fgo.simulator.models.variations;
 import lombok.AllArgsConstructor;
 import yome.fgo.data.proto.FgoStorageData.Target;
 import yome.fgo.simulator.models.Simulation;
+import yome.fgo.simulator.models.combatants.Combatant;
 import yome.fgo.simulator.utils.RoundUtils;
-import yome.fgo.simulator.utils.SpecificDamageUtils;
+import yome.fgo.simulator.utils.TargetUtils;
 
 @AllArgsConstructor
 public class TraitCountVariation implements Variation {
@@ -14,7 +15,27 @@ public class TraitCountVariation implements Variation {
 
     @Override
     public double evaluate(final Simulation simulation, final double baseValue, final double additionValue) {
-        final int count = SpecificDamageUtils.countTrait(simulation, target, targetTrait, maxCount);
+        final int count = countTrait(simulation, target, targetTrait, maxCount);
         return  RoundUtils.roundNearest(baseValue + additionValue * count);
+    }
+
+    public static int countTrait(
+            final Simulation simulation,
+            final Target target,
+            final String targetTrait,
+            final int maxCount
+    ) {
+        int count = 0;
+        for (final Combatant combatant : TargetUtils.getTargets(simulation, target)) {
+            for (final String trait : combatant.getAllTraits(simulation)) {
+                if (targetTrait.equalsIgnoreCase(trait)) {
+                    count++;
+                    if (maxCount == count) {
+                        return maxCount;
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
