ba1a38a29359e36512a99d5803440b86467f221f
Yome <695312637@qq.com>
2022-04-29 14:54:17 -0700

    should be part of previous commit

--- src/main/java/yome/fgo/simulator/models/conditions/ServantsExplodable.java
+++ src/main/java/yome/fgo/simulator/models/conditions/ServantsExplodable.java
@@ -1,11 +1,13 @@
 package yome.fgo.simulator.models.conditions;
 
-import lombok.AllArgsConstructor;
 import yome.fgo.simulator.models.Simulation;
 import yome.fgo.simulator.models.combatants.Combatant;
 
-@AllArgsConstructor
 public class ServantsExplodable implements Condition {
+    public static final ServantsExplodable EXPLOOOOOOOOOSION = new ServantsExplodable();
+
+    private ServantsExplodable() {}
+
     @Override
     public boolean evaluate(final Simulation simulation) {
         for (final Combatant combatant : simulation.currentServants) {
