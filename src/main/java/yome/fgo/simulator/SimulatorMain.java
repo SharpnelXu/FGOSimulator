d6604586776e47650b0812bc64f8b97ed6863e8c
Yome <695312637@qq.com>
2022-05-16 14:27:33 -0700

    [Lang] LangConfig

--- src/main/java/yome/fgo/simulator/SimulatorMain.java
+++ src/main/java/yome/fgo/simulator/SimulatorMain.java
@@ -1,11 +1,26 @@
 package yome.fgo.simulator;
 
+import org.apache.commons.configuration2.INIConfiguration;
 import yome.fgo.simulator.gui.creators.MainMenu;
 import yome.fgo.simulator.translation.TranslationManager;
 
+import java.io.FileReader;
+
+import static yome.fgo.simulator.utils.FilePathUtils.USER_DIR;
+
 public class SimulatorMain {
     public static void main(String[] args) {
-        TranslationManager.setTranslations("zh_CN");
+        final INIConfiguration options = new INIConfiguration();
+        options.setSeparatorUsedInInput("=");
+
+        try {
+            options.read(new FileReader(USER_DIR + "/options.ini"));
+            final String langString = options.getSection("Language").getString("lang");
+            System.out.println(langString);
+            TranslationManager.setTranslations(langString);
+        } catch (final Exception e) {
+            TranslationManager.setTranslations("zh_CN");
+        }
 
         MainMenu.main(args);
     }
