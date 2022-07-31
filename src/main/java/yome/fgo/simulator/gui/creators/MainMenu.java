c8918de80fe75fd79c1eab977f0b36e3b28aac9e
Yome <695312637@qq.com>
2022-07-31 14:17:56 -0700

    Update MainMenu.java

--- src/main/java/yome/fgo/simulator/gui/creators/MainMenu.java
+++ src/main/java/yome/fgo/simulator/gui/creators/MainMenu.java
@@ -23,7 +23,7 @@ public class MainMenu extends Application {
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.getIcons().add(new Image(new FileInputStream(ResourceManager.getCCThumbnail("default"))));
 
-        stage.setTitle(TranslationManager.getTranslation(APPLICATION_SECTION, "FGO Simulator") + " v0.61.beta");
+        stage.setTitle(TranslationManager.getTranslation(APPLICATION_SECTION, "FGO Simulator") + " v0.62.beta");
         stage.setScene(scene);
         stage.show();
     }
